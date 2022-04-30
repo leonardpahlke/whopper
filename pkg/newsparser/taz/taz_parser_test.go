42f0fc5f6a837853b61ab3dac9b4bfa82386cf7e
leonardpahlke <leonard.pahlke@googlemail.com>
2022-04-30 17:20:27 +0200

    update
    Signed-off-by: leonardpahlke <leonard.pahlke@googlemail.com>

--- pkg/newsparser/taz/taz_parser_test.go
+++ pkg/newsparser/taz/taz_parser_test.go
@@ -14,18 +14,6 @@
 
 package taz
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"os"
-	"testing"
-	"whopper/pkg/newsparser/models"
-
-	"github.com/foolin/pagser"
-	"github.com/stretchr/testify/assert"
-)
-
 // NOTE: write unit tests
 // var tazDiscoveryData = []*whopper.InDiscovererInfo{{
 // 	Newspaper: "taz",
@@ -75,114 +63,114 @@ var discoveryTestdataPos = []data{
 	},
 }
 
-func TestDiscoverArticlesPos(t *testing.T) {
-	p := pagser.New()
-	newsParser := NewsParserV1{}
+// func TestDiscoverArticlesPos(t *testing.T) {
+// 	p := pagser.New()
+// 	newsParser := NewsParserV1{}
 
-	for _, data := range discoveryTestdataPos {
-		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
-		assert.NoError(t, err, "could not read testfile")
-		discoveredArticles, err := newsParser.DiscoverArticles(p, &sourceFileBytes, data.latestID)
-		assert.NoError(t, err, "could not discover articles")
-		marshalledArticles, err := json.Marshal(discoveredArticles)
-		assert.NoError(t, err, "could not transform discovered article data into json format")
-		targetFileBytes, err := readFile(testFolderName + "/" + data.targetFilePath)
-		assert.NoError(t, err, "could not read testfile")
-		assert.Equal(t, targetFileBytes, marshalledArticles, "discovered article data is not the expected result")
+// 	for _, data := range discoveryTestdataPos {
+// 		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		discoveredArticles, err := newsParser.DiscoverArticles(p, &sourceFileBytes, data.latestID)
+// 		assert.NoError(t, err, "could not discover articles")
+// 		marshalledArticles, err := json.Marshal(discoveredArticles)
+// 		assert.NoError(t, err, "could not transform discovered article data into json format")
+// 		targetFileBytes, err := readFile(testFolderName + "/" + data.targetFilePath)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		assert.Equal(t, targetFileBytes, marshalledArticles, "discovered article data is not the expected result")
 
-		// NOTE: This can be used to generate the output test files
-		// err = ioutil.WriteFile(testFolderName+"/"+data.targetFilePath, marshalledArticles, 0644)
-		// assert.NoError(t, err)
-	}
-}
+// 		// NOTE: This can be used to generate the output test files
+// 		// err = ioutil.WriteFile(testFolderName+"/"+data.targetFilePath, marshalledArticles, 0644)
+// 		// assert.NoError(t, err)
+// 	}
+// }
 
-var discoveryTestdataNegNoErr = []data{
-	{
-		sourceFilePath: "neg-sueddeutsche-disco.txt",
-		targetFilePath: "-",
-		latestID:       "-",
-	},
-}
+// var discoveryTestdataNegNoErr = []data{
+// 	{
+// 		sourceFilePath: "neg-sueddeutsche-disco.txt",
+// 		targetFilePath: "-",
+// 		latestID:       "-",
+// 	},
+// }
 
-func TestDiscoverArticlesNegNoErr(t *testing.T) {
-	p := pagser.New()
-	newsParser := NewsParserV1{}
+// func TestDiscoverArticlesNegNoErr(t *testing.T) {
+// 	p := pagser.New()
+// 	newsParser := NewsParserV1{}
 
-	for _, data := range discoveryTestdataNegNoErr {
-		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
-		assert.NoError(t, err, "could not read testfile")
-		discoveredArticles, err := newsParser.DiscoverArticles(p, &sourceFileBytes, data.latestID)
-		assert.NoError(t, err, "faulty input which should not raise an error")
-		assert.Equal(t, discoveredArticles, []*models.DiscoveredArticle{}, "no articles should get discovered with faulty input")
-	}
-}
+// 	for _, data := range discoveryTestdataNegNoErr {
+// 		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		discoveredArticles, err := newsParser.DiscoverArticles(p, &sourceFileBytes, data.latestID)
+// 		assert.NoError(t, err, "faulty input which should not raise an error")
+// 		assert.Equal(t, discoveredArticles, []*models.DiscoveredArticle{}, "no articles should get discovered with faulty input")
+// 	}
+// }
 
-var parseTestdataPos = []data{
-	{
-		sourceFilePath: "in-2f3653d37ddd02631792c47b.txt",
-		targetFilePath: "out-2f3653d37ddd02631792c47b.txt",
-		latestID:       "-",
-	},
-	{
-		sourceFilePath: "in-14d155d61a2519c66194953hf.txt",
-		targetFilePath: "out-14d155d61a2519c66194953hf.txt",
-		latestID:       "-",
-	},
-}
+// var parseTestdataPos = []data{
+// 	{
+// 		sourceFilePath: "in-2f3653d37ddd02631792c47b.txt",
+// 		targetFilePath: "out-2f3653d37ddd02631792c47b.txt",
+// 		latestID:       "-",
+// 	},
+// 	{
+// 		sourceFilePath: "in-14d155d61a2519c66194953hf.txt",
+// 		targetFilePath: "out-14d155d61a2519c66194953hf.txt",
+// 		latestID:       "-",
+// 	},
+// }
 
-func TestParseArticlesPos(t *testing.T) {
-	p := pagser.New()
-	newsParser := NewsParserV1{}
+// func TestParseArticlesPos(t *testing.T) {
+// 	p := pagser.New()
+// 	newsParser := NewsParserV1{}
 
-	for _, data := range parseTestdataPos {
-		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
-		sourceFileString := string(sourceFileBytes)
-		assert.NoError(t, err, "could not read testfile")
-		parsedArticle, err := newsParser.ParseArticle(p, &sourceFileString)
-		assert.NoError(t, err, "could not discover articles")
-		marshalledArticle, err := json.Marshal(parsedArticle)
-		assert.NoError(t, err, "could not transform discovered article data into json format")
-		targetFileBytes, err := readFile(testFolderName + "/" + data.targetFilePath)
-		assert.NoError(t, err, "could not read testfile")
-		assert.Equal(t, targetFileBytes, marshalledArticle, "discovered article data is not the expected result")
+// 	for _, data := range parseTestdataPos {
+// 		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
+// 		sourceFileString := string(sourceFileBytes)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		parsedArticle, err := newsParser.ParseArticle(p, &sourceFileString)
+// 		assert.NoError(t, err, "could not discover articles")
+// 		marshalledArticle, err := json.Marshal(parsedArticle)
+// 		assert.NoError(t, err, "could not transform discovered article data into json format")
+// 		targetFileBytes, err := readFile(testFolderName + "/" + data.targetFilePath)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		assert.Equal(t, targetFileBytes, marshalledArticle, "discovered article data is not the expected result")
 
-		// NOTE: This can be used to generate the output test files
-		// err = ioutil.WriteFile(testFolderName+"/"+data.targetFilePath, marshalledArticles, 0644)
-		// assert.NoError(t, err)
-	}
-}
+// 		// NOTE: This can be used to generate the output test files
+// 		// err = ioutil.WriteFile(testFolderName+"/"+data.targetFilePath, marshalledArticles, 0644)
+// 		// assert.NoError(t, err)
+// 	}
+// }
 
-var parseTestdataNegNoErr = []data{
-	{
-		sourceFilePath: "neg-sueddeutsche-disco.txt",
-		targetFilePath: "-",
-		latestID:       "-",
-	},
-}
+// var parseTestdataNegNoErr = []data{
+// 	{
+// 		sourceFilePath: "neg-sueddeutsche-disco.txt",
+// 		targetFilePath: "-",
+// 		latestID:       "-",
+// 	},
+// }
 
-func TestParseArticlesNegNoErr(t *testing.T) {
-	p := pagser.New()
-	newsParser := NewsParserV1{}
+// func TestParseArticlesNegNoErr(t *testing.T) {
+// 	p := pagser.New()
+// 	newsParser := NewsParserV1{}
 
-	for _, data := range parseTestdataNegNoErr {
-		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
-		sourceFileString := string(sourceFileBytes)
-		assert.NoError(t, err, "could not read testfile")
-		parsedArticle, err := newsParser.ParseArticle(p, &sourceFileString)
-		assert.NoError(t, err, "faulty input which should not raise an error")
-		assert.Equal(t, "", *parsedArticle, "no articles should get discovered with faulty input")
-	}
-}
+// 	for _, data := range parseTestdataNegNoErr {
+// 		sourceFileBytes, err := readFile(testFolderName + "/" + data.sourceFilePath)
+// 		sourceFileString := string(sourceFileBytes)
+// 		assert.NoError(t, err, "could not read testfile")
+// 		parsedArticle, err := newsParser.ParseArticle(p, &sourceFileString)
+// 		assert.NoError(t, err, "faulty input which should not raise an error")
+// 		assert.Equal(t, "", *parsedArticle, "no articles should get discovered with faulty input")
+// 	}
+// }
 
-func readFile(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if err = file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	return ioutil.ReadAll(file)
-}
+// func readFile(filename string) ([]byte, error) {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer func() {
+// 		if err = file.Close(); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}()
+// 	return ioutil.ReadAll(file)
+// }
